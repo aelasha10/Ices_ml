6a4e2c684383e3b7876f10919b19f8e501610e94
aelasha10 <apekchhapoudel5@gmail.com>
2026-01-09 21:39:48 +0545

    Created folder and learn list and array

---
+++ numppy_day1/class.py
@@ -0,0 +1,12 @@
+
+import numpy as np # Importing the NumPy library
+
+py_list = [1, 2, 3] # Creating a Python list
+np_array = np.array([1, 2, 3]) # Creating a NumPy array
+
+print(py_list * 2) # Multiplying the Python list by 2
+print(np_array * 2)
+""""expected output:
+[1, 2, 3, 1, 2, 3]
+[2 4 6]
+"""
